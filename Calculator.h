c2819f0c817d1399883b0e3a92c8b13227a77ab4
Vincent Douville <vincent.douville9@gmail.com>
2026-03-17 17:18:32 -0400

    Init of files and header

---
+++ Calculator.h
@@ -0,0 +1,10 @@
+// header file for the Calculator class
+
+#pragma once
+
+class Calculator {
+public:
+    
+    // function to perform mathematical operations
+    double Calculate(double x, char oper, double y);
+};
